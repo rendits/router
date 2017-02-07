7810e30ac364a9f78a0b4d4121fea29c45c3123c
Albin Severinson <albin@severinson.org>
2017-02-07 19:45:59 +0100

    Refactoring

--- router/src/test/java/com/rendits/router/RouterTest.java
+++ router/src/test/java/com/rendits/router/RouterTest.java
@@ -8,6 +8,7 @@ import java.net.SocketAddress;
 import java.net.InetAddress;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+import java.net.SocketTimeoutException;
 import java.util.Properties;
 import java.io.IOException;
 import net.gcdc.UdpDuplicator;
@@ -16,6 +17,45 @@ public class RouterTest extends TestCase {
 
         public RouterTest(String testName) {
                 super(testName);
+        }
+
+        private void sendMessages(DatagramSocket socket, InetAddress routerAddress, int portRcvFromVehicle) throws IOException {
+                byte[] buffer;
+                DatagramPacket packet;
+                DatagramPacket rcvPacket;
+                int messagesToSend = 100;
+                SimpleCam simpleCam = SampleMessages.getSampleCam();
+                SimpleDenm simpleDenm = SampleMessages.getSampleDenm();
+                SimpleIclcm simpleIclcm = SampleMessages.getSampleIclcm();
+                for (int i = 0; i < messagesToSend; i++) {
+                        buffer = simpleCam.asByteArray();
+                        packet = new DatagramPacket(buffer, buffer.length);
+                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
+                        packet.setPort(portRcvFromVehicle);
+                        packet.setAddress(routerAddress);
+                        socket.send(packet);
+                        socket.receive(rcvPacket);
+                        assertEquals(simpleCam, new SimpleCam(rcvPacket.getData()));
+
+                        buffer = simpleDenm.asByteArray();
+                        packet = new DatagramPacket(buffer, buffer.length);
+                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
+                        packet.setPort(portRcvFromVehicle);
+                        packet.setAddress(routerAddress);
+                        socket.send(packet);
+                        socket.receive(rcvPacket);
+                        assertEquals(simpleDenm, new SimpleDenm(rcvPacket.getData()));
+
+                        buffer = simpleIclcm.asByteArray();
+                        packet = new DatagramPacket(buffer, buffer.length);
+                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
+                        packet.setPort(portRcvFromVehicle);
+                        packet.setAddress(routerAddress);
+                        socket.send(packet);
+                        socket.receive(rcvPacket);
+                        assertEquals(simpleIclcm, new SimpleIclcm(rcvPacket.getData()));
+                }
+                return;
         }
 
         public void testIntegrity() throws IOException {
@@ -51,83 +91,19 @@ public class RouterTest extends TestCase {
 
                 /* Setup sockets */
                 DatagramSocket socket = new DatagramSocket(portSendIts);
-                socket.setSoTimeout(10000);
-                byte[] buffer;
-                DatagramPacket packet;
-                DatagramPacket rcvPacket;
+                socket.setSoTimeout(1000);
                 InetAddress routerAddress = InetAddress.getByName("127.0.0.1");
 
                 /* Send some messages and make sure we get the same
                  * thing back. */
-                for (int i = 0; i < 100; i++) {
-                        SimpleCam simpleCam = SampleMessages.getSampleCam();
-                        buffer = simpleCam.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleCam, new SimpleCam(rcvPacket.getData()));
-
-                        SimpleDenm simpleDenm = SampleMessages.getSampleDenm();
-                        buffer = simpleDenm.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleDenm, new SimpleDenm(rcvPacket.getData()));
-
-                        SimpleIclcm simpleIclcm = SampleMessages.getSampleIclcm();
-                        buffer = simpleIclcm.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleIclcm, new SimpleIclcm(rcvPacket.getData()));
-                }
+                sendMessages(socket, routerAddress, portRcvFromVehicle);
 
                 /* Stop the router */
                 router.close();
 
                 /* Start it again and make sure everything still works. */
                 router = new Router(props);
-                for (int i = 0; i < 100; i++) {
-                        SimpleCam simpleCam = SampleMessages.getSampleCam();
-                        buffer = simpleCam.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleCam, new SimpleCam(rcvPacket.getData()));
-
-                        SimpleDenm simpleDenm = SampleMessages.getSampleDenm();
-                        buffer = simpleDenm.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleDenm, new SimpleDenm(rcvPacket.getData()));
-
-                        SimpleIclcm simpleIclcm = SampleMessages.getSampleIclcm();
-                        buffer = simpleIclcm.asByteArray();
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        rcvPacket = new DatagramPacket(new byte[buffer.length], buffer.length);
-                        packet.setPort(portRcvFromVehicle);
-                        packet.setAddress(routerAddress);
-                        socket.send(packet);
-                        socket.receive(rcvPacket);
-                        assertEquals(simpleIclcm, new SimpleIclcm(rcvPacket.getData()));
-                }
-
+                sendMessages(socket, routerAddress, portRcvFromVehicle);
                 router.close();
         }
 }
