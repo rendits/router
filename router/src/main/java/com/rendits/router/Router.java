5203a7aee02fd28fc215b50137ae675ba640c7ab
Albin Severinson <albin@severinson.org>
2018-03-04 10:56:41 +0100

    updated copyright notice

--- router/src/main/java/com/rendits/router/Router.java
+++ router/src/main/java/com/rendits/router/Router.java
@@ -1,16 +1,16 @@
-/* Copyright 2016 Albin Severinson
+/* Copyright 2018 Rendits
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.rendits.router;
@@ -331,7 +331,7 @@ public class Router {
                     + vehicleIclcmPort
                     + "\nSending incoming custom messages to port "
                     + vehicleCustomPort
-                    + "\nCopyright: Albin Severinson (albin@rendits.com)");
+                    + "\nCopyright: Rendits (albin@rendits.com)");
 
             /* Log statistics every second */
             while (running) {
