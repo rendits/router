31c2d4f3200114583a7e616c3bfd849236c0395b
Albin Severinson <albin@severinson.org>
2018-03-04 10:43:06 +0100

    license, file comment

--- test-suite/src/main/java/com/rendits/testsuite/TestSuite.java
+++ test-suite/src/main/java/com/rendits/testsuite/TestSuite.java
@@ -1,6 +1,20 @@
-/* Copyright 2016 Albin Severinson
- * Test suite for the Rendits vehicle router. Includes test for
- * latency, packet loss, duplicates, etc.
+/* Copyright 2018 Rendits
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/* Test suite for the Rendits V2X router. Meant to verify that the router is
+ * working properly and to measure the transmission latency.
  */
 
 package com.rendits.testsuite;
