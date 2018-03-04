50a7272ab7f276b2d10913f6ad45cafcbf39d342
Albin Severinson <albin@severinson.org>
2018-03-04 10:34:50 +0100

    comment, indentation

--- test-suite/src/main/java/com/rendits/testsuite/Vehicle.java
+++ test-suite/src/main/java/com/rendits/testsuite/Vehicle.java
@@ -7,21 +7,22 @@ package com.rendits.testsuite;
 import java.time.Instant;
 
 public class Vehicle {
-        private final int stationID;
+    private final int stationID;
 
-        Vehicle(int stationID) {
-                this.stationID = stationID;
-        }
+    Vehicle(int stationID) {
+        this.stationID = stationID;
+    }
 
-        public int getStationID() {
-                return this.stationID;
-        }
+    public int getStationID() {
+        return this.stationID;
+    }
 
-        /* TODO: Does this take leap seconds etc into account correctly? */
-        public static int getGenerationDeltaTime(){
-                Instant instant = Instant.now();
-                long generationDeltaTime = (instant.getEpochSecond()*1000 +
-                                            instant.getNano()/1000000) % 65536;
-                return (int) generationDeltaTime;
-        }
+    /* Not necessarily compliant with ITS-G5 spec. in how leap seconds are
+     * counted etc. But enough to test the router. */
+    public static int getGenerationDeltaTime(){
+        Instant instant = Instant.now();
+        long generationDeltaTime = (instant.getEpochSecond()*1000 +
+                                    instant.getNano()/1000000) % 65536;
+        return (int) generationDeltaTime;
+    }
 }
